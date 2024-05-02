dd0ecb401a53e034232a989029ce8f088bdeadeb
Akshay Badagabettu <74955326+akshay140601@users.noreply.github.com>
2024-05-01 21:55:10 -0400

    Update llm.py

--- src/llm.py
+++ src/llm.py
@@ -53,3 +53,48 @@ def get_answers(model, api_key, query, temp, base_url = None) -> str:
         print(output)
 
         return output
+    
+    elif model == 'gpt4-turbo':
+        client = OpenAI(api_key=api_key, base_url=base_url)
+        message = {
+            'role' : 'user',
+            'content' : query
+        }
+        response = client.chat.completions.create(
+            model="gpt-4-turbo",
+            messages=[message],
+            stop=["<END>", "### [User message]", "\n\n\n\n\n"],
+            temperature=temp,
+            max_tokens=3000
+        )
+        output = response.choices[0].message.content
+        print(output)
+
+        return output
+
+    
+    elif model == "llama3":
+        os.environ["TOGETHER_API_KEY"] = api_key
+        url = 'https://api.together.xyz/inference'
+        headers = {
+        'Authorization': 'Bearer ' + os.environ["TOGETHER_API_KEY"],
+        'accept': 'application/json',
+        'content-type': 'application/json'
+        }
+        data = {
+        "model": "meta-llama/Llama-3-70b-chat-hf",
+        "prompt": query,
+        "max_tokens": 3000,
+        "temperature": temp,
+        "top_p": 0.7,
+        "top_k": 50,
+        "repetition_penalty": 1,
+        "stop": ["<|EOT|>","<|begin▁of▁sentence|>","<|end▁of▁sentence|>", "<|\s|>", "### [User message]", "\n\n\n\n\n"]
+        }
+        response = requests.post(url, json=data, headers=headers)
+        #print(response.json())
+        #print('===================')
+        text = response.json()['output']['choices'][0]['text']
+        print(text)
+
+        return text
